1916dbd3d6f83a4ab62d7023f1601bef1de166c4
Alexandru Gumeniuc <alexandru.gumeniuc@endava.com>
2020-08-04 22:14:47 +0300

    Added logstash logging

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,7 +1,8 @@
 package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,14 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		Greeting response = new Greeting(counter.incrementAndGet(), String.format(template, name));
+		log.info("{\"id\":{},\"content\":\"{}\"}", response.getId(), response.getContent());
+		//log.error("No luck !", new RuntimeException("Crash made a runtime exception"));
+
+		return response;
 	}
 }
